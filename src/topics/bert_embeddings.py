3b61d6a4bcaae0f4d9204b770e803cfb35654172
Mukul Ranjan <mukulranjaniitg@gmail.com>
2021-10-04 07:44:36 +0530

    update bert_embeddings

--- src/topics/bert_embeddings.py
+++ src/topics/bert_embeddings.py
@@ -15,3 +15,17 @@ def load_file(cleaned_text_path):
 
 	return data
 
+def set_seed(seed = 42):
+    '''Sets the seed of the entire notebook so results are the same every time we run.
+    This is for REPRODUCIBILITY.'''
+    np.random.seed(seed)
+    random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    # When running on the CuDNN backend, two further options must be set
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = True
+    Set a fixed value for the hash seed
+    os.environ['PYTHONHASHSEED'] = str(seed)
+set_seed()
+
